2ed244ae7cb960ef9ee509b7a98ffb9e41bfaf63
mrasamny <mrasamny@desu.edu>
2022-09-02 13:27:06 -0400

    First Commit

--- cypher.py
+++ cypher.py
@@ -2,18 +2,21 @@
 # CSCI-121 Elements of Computer Programming II
 # Recitation 1 - Encryption with a password
 # *******************************************************
-
 import string
 
 alphabet = string.printable
 ordinal_value = {ch: i for i, ch in enumerate(alphabet)}
 
-
 def encrypt(message, password):
     encrypted_message = ''
-    pass  # TODO replace this line with your code.
-
+    for index, ch in enumerate(message):
+        encrypted_ch = alphabet[(ordinal_value[ch] + ordinal_value [password[index % len(password)]]) % len(alphabet)]
+        encrypted_message += encrypted_ch
+    return encrypted_message
 
 def decrypt(message, password):
     decrypted_message = ''
-    pass  # TODO replace this line with your code.
+    for index, ch in enumerate(message):
+        decrypted_ch = alphabet[(ordinal_value[ch] - ordinal_value[password[index % len(password)]]) % len(alphabet)]
+        decrypted_message += decrypted_ch
+    return decrypted_message
